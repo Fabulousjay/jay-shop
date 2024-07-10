2aebed909c5aeaae3f02dbaee7f889606b449a9b
adegbulu_ayomiposi <adegbuluayomiposi@gmail.com>
2024-07-10 01:00:44 +0100

    worked on the Cart Page

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -32,14 +32,37 @@ const cartSlice = createSlice({
 			}
 			state.numItemsInCart += product.amount;
 			state.cartTotal += product.price * product.amount;
+			cartSlice.caseReducers.calculateTotals(state);
+
+			toast.success('Items Added to Cart Successfully');
+		},
+		clearCart: (state) => {
+			localStorage.setItem('cart', JSON.stringify(defaultState));
+			return defaultState;
+		},
+
+		removeItem: (state, action) => {
+			const { cartID } = action.payload;
+			const product = state.cartItems.find((i) => i.cartID === cartID);
+			state.numItemsInCart -= product.amount;
+			cartSlice.caseReducers.calculateTotals(state);
+			toast.error('Item removed from cart');
+		},
+		editItem: (state, cart) => {
+			const { cartID, amount } = action.payload;
+			const item = state.cartItems.find((i) => i.cartID === cartID);
+			state.numItemsInCart += amount - item.amount;
+			state.cartTotal += item.price * (amount - item.amount);
+			item.amount = amount;
+			cartSlice.caseReducers.calculateTotals(state);
+			toast.success('Cart Updated');
+		},
+		calculateTotals: () => {
+			//since we are gonna be using the three lines of code below
 			state.tax = 0.1 * state.cartTotal;
 			state.orderTotal = state.cartTotal + state.shipping + state.tax;
 			localStorage.setItem('cart', JSON.stringify(state));
-			toast.success('Items Added to Cart Successfully');
 		},
-		clearCart: (state) => {},
-		removeItem: (state, action) => {},
-		editItem: (state, cart) => {},
 	},
 });
 
