8bc31c0c4f834936c0f0e255b599ec7903329375
adegbulu_ayomiposi <adegbuluayomiposi@gmail.com>
2024-07-13 23:26:37 +0100

    worked on cart items and functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 
 const defaultState = {
 	cartItems: [],
-	numItemsInCart: 10,
+	numItemsInCart: 0,
 	cartTotal: 0,
 	shipping: 500, //in cent
 	tax: 0,
@@ -23,16 +23,20 @@ const cartSlice = createSlice({
 	reducers: {
 		addItem: (state, action) => {
 			const { product } = action.payload;
-			const item = state.cartItems.find((i) => i.cardID === product.cardID);
+
+			// Check if the item already exists in the cart
+			const item = state.cartItems.find((i) => i.cartID === product.cartID);
 
 			if (item) {
+				// If item exists, update the amount
 				item.amount += product.amount;
 			} else {
 				state.cartItems.push(product);
 			}
-
+			// Update cart totals
 			state.numItemsInCart += product.amount;
 			state.cartTotal += product.price * product.amount;
+			// Recalculate tax and order total
 			cartSlice.caseReducers.calculateTotals(state);
 
 			toast.success('Items Added to Cart Successfully');
@@ -45,11 +49,13 @@ const cartSlice = createSlice({
 		removeItem: (state, action) => {
 			const { cartID } = action.payload;
 			const product = state.cartItems.find((i) => i.cartID === cartID);
+			state.cartItems = state.cartItems.filter((i) => i.cartID !== cartID);
 			state.numItemsInCart -= product.amount;
+			state.cartTotal -= product.price * product.amount;
 			cartSlice.caseReducers.calculateTotals(state);
 			toast.error('Item removed from cart');
 		},
-		editItem: (state, cart) => {
+		editItem: (state, action) => {
 			const { cartID, amount } = action.payload;
 			const item = state.cartItems.find((i) => i.cartID === cartID);
 			state.numItemsInCart += amount - item.amount;
@@ -58,7 +64,7 @@ const cartSlice = createSlice({
 			cartSlice.caseReducers.calculateTotals(state);
 			toast.success('Cart Updated');
 		},
-		calculateTotals: () => {
+		calculateTotals: (state) => {
 			//since we are gonna be using the three lines of code below
 			state.tax = 0.1 * state.cartTotal;
 			state.orderTotal = state.cartTotal + state.shipping + state.tax;
---
+++ src/features/user/userslice.js
@@ -0,0 +1,28 @@
+/** @format */
+
+import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+
+const initialState = {
+	user: { username: 'Adegbulu ayo' },
+	theme: 'dark',
+};
+
+const userSlice = createSlice({
+	name: 'user',
+	initialState,
+	reducers: {
+		loginUser: (state, action) => {
+			console.log('login');
+		},
+		logoutUser: (state) => {
+			console.log('logout');
+		},
+		toggleTheme: (state) => {
+			console.log('toggle theme');
+		},
+	},
+});
+
+export const { loginUser, logoutUser, toggleTheme } = userSlice.actions;
+export default userSlice.reducer;
