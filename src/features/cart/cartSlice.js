6166a754027fb3fb9d39c92a26bc83cc41af9d9d
Adegbulu Ayomiposi <Adegbuluayomiposi@gmail.com>
2025-01-11 15:27:08 +0100

    removed unnecassry commented codes

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -7,7 +7,7 @@ const defaultState = {
 	cartItems: [],
 	numItemsInCart: 0,
 	cartTotal: 0,
-	shipping: 500, //in cent
+	shipping: 500,
 	tax: 0,
 	orderTotal: 0,
 };
@@ -18,25 +18,21 @@ const getCartFromLocalStorge = () => {
 
 const cartSlice = createSlice({
 	name: 'cart',
-	// initialState: defaultState,
 	initialState: getCartFromLocalStorge(),
 	reducers: {
 		addItem: (state, action) => {
 			const { product } = action.payload;
-
-			// Check if the item already exists in the cart
 			const item = state.cartItems.find((i) => i.cartID === product.cartID);
 
 			if (item) {
-				// If item exists, update the amount
 				item.amount += product.amount;
 			} else {
 				state.cartItems.push(product);
 			}
-			// Update cart totals
+
 			state.numItemsInCart += product.amount;
 			state.cartTotal += product.price * product.amount;
-			// Recalculate tax and order total
+
 			cartSlice.caseReducers.calculateTotals(state);
 
 			toast.success('Items Added to Cart Successfully');
@@ -65,7 +61,6 @@ const cartSlice = createSlice({
 			toast.success('Cart Updated');
 		},
 		calculateTotals: (state) => {
-			//since we are gonna be using the three lines of code below
 			state.tax = 0.1 * state.cartTotal;
 			state.orderTotal = state.cartTotal + state.shipping + state.tax;
 			localStorage.setItem('cart', JSON.stringify(state));
